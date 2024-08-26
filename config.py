bc83600f31c6aa20988339b4d6b20d9bfcf54869
Tonnfu22 <95202310+Tonnfu22@users.noreply.github.com>
2024-08-26 14:18:47 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("7278576205:AAH0iW-ftAvjlnaIv0QrRasbAP6WG-09gd4")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 CRYPTO_API_KEY = os.getenv("CRYPTO_API_KEY")
